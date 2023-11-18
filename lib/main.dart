af381ac0a2cedd7fae671c60f0c22fbb3629ea45
Alejandro Egas <alejoegasg@outlook.com>
2023-11-18 13:13:49 -0500

    Quitar la Bandera del Costado

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'StayHub',
+      debugShowCheckedModeBanner:false,
       theme: ThemeData(
         primarySwatch: Colors.amber
       ),
