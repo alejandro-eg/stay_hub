f43541f2a6deeeb304bb092713495db7f977fdc9
Alejandro Egas <alejoegasg@outlook.com>
2023-11-19 19:47:39 -0500

    Alineación

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:stay_hub/registrar_cliente.dart';
 import 'package:stay_hub/reservar_habitacion.dart';
 import 'package:stay_hub/consultar_reservas.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,10 +16,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'StayHub',
-      debugShowCheckedModeBanner:false,
-      theme: ThemeData(
-        primarySwatch: Colors.indigo
-      ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.indigo),
       home: const BienvenidaScreen(),
     );
   }
@@ -33,14 +32,14 @@ class BienvenidaScreen extends StatelessWidget {
         title: const Text('Bienvenidos a StayHub'),
       ),
       body: Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            Image.asset(
-              'assets/stayhub.png',
-              fit: BoxFit.cover,
-            ),
-            Align(
-             alignment: Alignment.bottomRight,
+        fit: StackFit.expand,
+        children: <Widget>[
+          Image.asset(
+            'assets/stayhub.png',
+            fit: BoxFit.cover,
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
             child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: ElevatedButton(
@@ -53,19 +52,22 @@ class BienvenidaScreen extends StatelessWidget {
                   );
                 },
                 style: ElevatedButton.styleFrom(
-                  primary: Color(0xff3c4c44), // Cambia el color del botón cuando se presiona
-                  minimumSize: Size(100, 25), // Cambia el tamaño mínimo del botón
-              ),
-              child: const Text('Ingresar',
-              style: TextStyle(
-                  fontSize: 30,
-                  color: Color(0xffe0bd6b), // Cambia el color del texto
+                  primary: Color(
+                      0xff3c4c44), // Cambia el color del botón cuando se presiona
+                  minimumSize:
+                      Size(100, 25), // Cambia el tamaño mínimo del botón
+                ),
+                child: const Text(
+                  'Ingresar',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Color(0xffe0bd6b), // Cambia el color del texto
+                  ),
                 ),
               ),
-              ),
             ),
-            ),
-            Align(
+          ),
+          Align(
             alignment: Alignment.bottomLeft,
             child: Padding(
               padding: const EdgeInsets.all(16.0),
@@ -73,50 +75,53 @@ class BienvenidaScreen extends StatelessWidget {
                 onPressed: () async {
                   // Código para el botón "Salir"
                   showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: const Text('Confirmación'),
-                        content: const Text(
-                            '¿Está seguro de que desea salir de la app?'),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context)
-                                  .pop(); // Cerrar el cuadro de diálogo
-                            },
-                            child: const Text('Cancelar'),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop(exit(0));
-                                   // Cerrar la app diálogo
-                            },
-                            child: const Text('Salir'),
-                          ),
-                          ],                  
-                          );
-                          }
-                  );
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text('Confirmación'),
+                          content: const Text(
+                              '¿Está seguro de que desea salir de la app?'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context)
+                                    .pop(); // Cerrar el cuadro de diálogo
+                              },
+                              child: const Text('Cancelar'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop(exit(0));
+                                // Cerrar la app diálogo
+                              },
+                              child: const Text('Salir'),
+                            ),
+                          ],
+                        );
+                      });
                 },
                 style: ElevatedButton.styleFrom(
-                  primary: Color(0xff3c4c44), // Cambia el color del botón cuando se presiona
-                  minimumSize: Size(100, 25), // Cambia el tamaño mínimo del botón
-              ),
-              child: const Text('Salir',
-              style: TextStyle(
-                  fontSize: 30,
-                  color: Color(0xffe0bd6b), // Cambia el color del texto
+                  primary: Color(
+                      0xff3c4c44), // Cambia el color del botón cuando se presiona
+                  minimumSize:
+                      Size(100, 25), // Cambia el tamaño mínimo del botón
                 ),
-              ),
+                child: const Text(
+                  'Salir',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Color(0xffe0bd6b), // Cambia el color del texto
+                  ),
+                ),
               ),
             ),
           ),
-          ],
-        ),
+        ],
+      ),
     );
   }
 }
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
@@ -127,79 +132,88 @@ class MyHomePage extends StatelessWidget {
         title: const Text('StayHub'),
       ),
       body: Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            Image.asset(
-              'assets/stayhub.png',
-              fit: BoxFit.cover,
-            ),
-        Align(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Image.asset(
+            'assets/stayhub.png',
+            fit: BoxFit.cover,
+          ),
+          Align(
             alignment: Alignment.bottomCenter,
             child: Padding(
               padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const RegistrarCliente(),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const RegistrarCliente(),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(
+                          0xff3c4c44), // Cambia el color del botón cuando se presiona
+                      minimumSize:
+                          Size(100, 25), // Cambia el tamaño mínimo del botón
+                    ),
+                    child: const Text(
+                      'Registráte',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Color(0xffe0bd6b), // Cambia el color del texto
+                      ),
+                    ),
                   ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                  primary: Color(0xff3c4c44), // Cambia el color del botón cuando se presiona
-                  minimumSize: Size(100, 25), // Cambia el tamaño mínimo del botón
-              ),
-              child: const Text('Registráte',
-              style: TextStyle(
-                  fontSize: 20,
-                  color: Color(0xffe0bd6b), // Cambia el color del texto
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ReservarHabitacion(),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ReservarHabitacion(),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(
+                          0xff3c4c44), // Cambia el color del botón cuando se presiona
+                      minimumSize:
+                          Size(100, 25), // Cambia el tamaño mínimo del botón
+                    ),
+                    child: const Text(
+                      'Reservar',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Color(0xffe0bd6b), // Cambia el color del texto
+                      ),
+                    ),
                   ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                  primary: Color(0xff3c4c44), // Cambia el color del botón cuando se presiona
-                  minimumSize: Size(100, 25), // Cambia el tamaño mínimo del botón
-              ),
-              child: const Text('Reservar',
-              style: TextStyle(
-                  fontSize: 20,
-                  color: Color(0xffe0bd6b), // Cambia el color del texto
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ConsultarReservas(),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ConsultarReservas(),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(
+                          0xff3c4c44), // Cambia el color del botón cuando se presiona
+                      minimumSize:
+                          Size(100, 25), // Cambia el tamaño mínimo del botón
+                    ),
+                    child: const Text(
+                      'Consultar Reserva',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Color(0xffe0bd6b), // Cambia el color del texto
+                      ),
+                    ),
                   ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                  primary: Color(0xff3c4c44), // Cambia el color del botón cuando se presiona
-                  minimumSize: Size(100, 25), // Cambia el tamaño mínimo del botón
-              ),
-              child: const Text('Consultar Reserva',
-              style: TextStyle(
-                  fontSize: 20,
-                  color: Color(0xffe0bd6b), // Cambia el color del texto
-                ),
-              ),
-            ),
                 ],
               ),
             ),
